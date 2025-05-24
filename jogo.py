a7ec11c03b8de1263ff04164dff2a9d58942ac08
Dids-fl <diegofla@al.insper.edu.br>
2025-05-24 17:56:12 -0300

    info fonte

--- jogo.py
+++ jogo.py
@@ -8,5 +8,16 @@ pygame.init()
 info = pygame.display.Info()
 tela_largura = info.current_w
 tela_altura = info.current_h
-tela = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-largura_tela, altura_tela = tela.get_size()
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+screen_width, screen_height = screen.get_size()
+
+def get_relative_dir(dir: str) -> str:
+    return Path(__file__).parent.joinpath(dir)
+
+num_faixas = 8
+largura_faixa = screen_width // num_faixas
+faixas_x = [i * largura_faixa + largura_faixa // 2 for i in range(num_faixas)]
+
+my_font = pygame.font.Font(get_relative_dir('Font/BACKTO1982.TTF'), 32)
+my_font_start = pygame.font.Font(get_relative_dir('Font/BACKTO1982.TTF'), 20)
+small_font = pygame.font.Font(get_relative_dir('Font/BACKTO1982.TTF'), 10)
